772674c874f0e48a20559502886568b5ea6f1352
mrrohanrp <rohanpote0@gmail.com>
2022-02-28 23:27:22 +0530

    Task/TA-003: add NoMatchPage

--- src/Pages/NoMatchPage.jsx
+++ src/Pages/NoMatchPage.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Logo } from '../components';
+
+import styles from './NoMatchPage.module.scss';
 
 const NoMatchPage = () => {
-  return <div>NoMatchPage</div>;
+  return (
+    <div className={styles.nomatch}>
+      <Logo />
+
+      <div className={styles.nomatch__container}>
+        <img />
+        <div className={styles.nomatch__content}>
+          <h1>Looking for something?</h1>
+          <p>We&apos;re sorry. The Web address you entered is not a functioning page on our site.</p>
+          <div className={styles.nomatch__content__end}>
+            <img />
+            <h2>
+              Go to Amazon.in&apos;s <Link to="/">Home</Link> Page
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { NoMatchPage };
